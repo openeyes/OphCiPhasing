d38d43a2f0cb9b243412689005d1fa4514396201
Richard Willis <willis.rh@gmail.com>
2014-01-23 14:26:54 +0000

    [OE-3955] Moved JS test files into tests dir; added connect grunt task for displaying the test runners (to allow for network operations in the tests)

--- tests/js/module.spec.js
+++ tests/js/module.spec.js
@@ -1,9 +1,8 @@
-/*
+
 describe('My module', function() {
 	describe('A feature', function() {
 		it('should do something', function() {
-			expect(something).to.equal(somethingElse);
+			expect(1).to.equal(1);
 		});
 	});
 });
-*/
