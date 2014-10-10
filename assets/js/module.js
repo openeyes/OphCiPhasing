5666045de0b71ee3b8fd2c55770a571879182ce5
Mark Wadham <m@rkw.me>
2014-10-10 11:16:07 +0100

    [OEM-255] use new printEvent method

--- assets/js/module.js
+++ assets/js/module.js
@@ -91,9 +91,8 @@ $(document).ready(function() {
 	});
 
 	handleButton($('#et_print'),function(e) {
-		printIFrameUrl(OE_print_url, null);
-		enableButtons();
 		e.preventDefault();
+		printEvent();
 	});
 });
 
